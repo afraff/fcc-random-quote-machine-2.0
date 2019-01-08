747df7f8705d73ecd9a5ff40d3bded8b60d41d22
Amy Rafferty <amyfrances.r@gmail.com>
2019-01-08 16:11:18 -0700

    Updates

---
+++ precache-manifest.d97ecff7e9c8dc8af02db624d66ebe57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba84ce454950d82f9c28",
+    "url": "/fcc-random-quote-machine-2.0/static/js/runtime~main.ba84ce45.js"
+  },
+  {
+    "revision": "483243e0e36d9ab9e562",
+    "url": "/fcc-random-quote-machine-2.0/static/js/main.483243e0.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/fcc-random-quote-machine-2.0/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "483243e0e36d9ab9e562",
+    "url": "/fcc-random-quote-machine-2.0/static/css/main.f74020d6.chunk.css"
+  },
+  {
+    "revision": "93bea11eec32d7e7287cebb5be7268d8",
+    "url": "/fcc-random-quote-machine-2.0/index.html"
+  }
+];
